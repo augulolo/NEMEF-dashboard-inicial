2fb969dc640cf81e1378563085a35b7a35acfb05
Agustin <aguslp2002@gmail.com>
2026-04-16 20:48:15 -0300

    fix logo - usar svg en lugar de png inexistente

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,6 +1,6 @@
 export function Logo({ className = "h-8 w-8" }: { className?: string }) {
   return (
     // eslint-disable-next-line @next/next/no-img-element
-    <img src="/nemef-logo.png" alt="NEMEF" className={className} />
+    <img src="/nemef-logo.svg" alt="NEMEF" className={className} />
   );
 }
